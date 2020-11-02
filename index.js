eb054894b866e95f8b377e9a9f8d040671eaa789
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-11-02 17:33:15 -0500

    added date

---
+++ index.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`
+}
+
+
+function init() {
+    getTime();
+}
+
+init();
