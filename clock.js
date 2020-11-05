b6d7da597056d8edcf71b3a58a2665ec92fd0275
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-11-05 15:43:24 -0500

    added input for the username

--- clock.js
+++ clock.js
@@ -7,7 +7,8 @@ function getTime() {
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+
 }
 
 
