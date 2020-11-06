2cfecdf72bea1baf6d13894b0b00476e2a66dd07
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-11-05 20:27:29 -0500

    added delete button

--- todo.js
+++ todo.js
@@ -4,7 +4,20 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -18,6 +31,7 @@ function paintToDo(text) {
 
 
     delBtn.innerHTML = "&#10060";
+    delBtn.addEventListener("click", deleteToDo);
     
     span.innerText = text;
     li.appendChild(delBtn);
